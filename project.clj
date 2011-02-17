197bb71a9803cdf36ecadac3850ecbe1d3b5cb47
David Li <taweili@yahoo.com>
2011-02-17 12:22:05 +0800

    update project

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject lein-notes "0.0.1"
   :description "Simple Leiningen plugin to read inline notes in the source codes"
+  :eval-in-leiningen true
+  :dev-dependencies [[lein-clojars "0.6.0"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])

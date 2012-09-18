cb1f8579154d318db90e1fb5b3b53053319950a1
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-09-18 11:49:33 +0700

    make usable in lein 2.x

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject lein-notes "0.0.1"
+(defproject lein-notes "0.0.2"
   :description "Simple Leiningen plugin to read inline notes in the source codes"
   :eval-in-leiningen true
-  :dev-dependencies [[lein-clojars "0.6.0"]]
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/leiningen/notes.clj
+++ src/leiningen/notes.clj
@@ -1,19 +1,28 @@
 (ns leiningen.notes
   "Print all the FIXME, TODO, OPTIMIZE in the source files with line numbers"
-  (:use [clojure.contrib.duck-streams :only [read-lines]])
-  (:use [clojure.contrib.seq-utils :only [indexed]])
-  (:use [clojure.java.io :only [file]]))
+  (:use [clojure.java.io :only [file reader]]))
+
+;; copy from clojure.contrib.seq-utils
+(defn- indexed
+  "Returns a lazy sequence of [index, item] pairs, where items come
+from 's' and indexes count up from zero.
+
+(indexed '(a b c d)) => ([0 a] [1 b] [2 c] [3 d])"
+  [s]
+  (map vector (iterate inc 0) s))
+
 
 (defn- print-note [file, line, count]
   (if (re-find #"FIXME|OPTIMIZE|TODO|REVISIT|XXX" line)
     (println (str (. file getPath) ":" (+ 1 count) "   " line))))
 
 (defn- find-notes [f]
-  (dorun (map #(print-note f (second %) (first %)) (indexed (read-lines f)))))
+  (dorun (map #(print-note f (second %) (first %))
+              (indexed (line-seq (reader f))))))
 
-(defn notes 
+(defn notes
   [project & args]
   (let [files (filter #(not (or (. % isDirectory)
-				(re-find #"/\.git/|/lib/|/classes/" (. % getPath))))
-		      (file-seq (file (project :root))))]
+                                (re-find #"/\.git/|/lib/|/classes/" (. % getPath))))
+                      (file-seq (file (project :root))))]
     (dorun (map find-notes files))))
